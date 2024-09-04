c920b811c5575bf8ec5f0b43b9b5bd7226c191c6
IacopoDiPippo <dipippo.1966046@studenti.uniroma1.it>
2024-09-04 16:17:52 +0200

    a
    aa

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ from tqdm import tqdm
 from torch.utils.data import Dataset
 
 from sklearn.ensemble import RandomForestClassifier
-
+os.chdir('C:/Users/iacop/Documents/GitHub/Kaggle1/Titanic_Kaggle')
 train_data =pd.read_csv("titanic/train.csv")
 test_data =pd.read_csv("titanic/test.csv")
 
@@ -24,7 +24,7 @@ y = train_data["Survived"]
 features = ["Pclass", "Sex", "SibSp", "Parch"]
 X = pd.get_dummies(train_data[features])
 X_test = pd.get_dummies(test_data[features])
-
+print(X.head())
 model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
 model.fit(X, y)
 predictions = model.predict(X_test)
